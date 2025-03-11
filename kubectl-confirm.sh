ba42a7b39f361465744059f1a757adc248cab726
Slzzp Wu <slzzpwu@gamania.com>
2025-03-11 12:42:00 +0000

    support delete namespace

--- kubectl-confirm.sh
+++ kubectl-confirm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# support syntax example
+# support syntax
 # - kubectl delete namespace foobar
 
 # kubectl location
@@ -9,6 +9,32 @@ KUBECTL=/usr/local/bin/kubectl
 COMMAND=$1
 
 case "${COMMAND}" in
+  "delete")
+    shift
+    RESOURCE_TYPE=$1
+
+    case "${RESOURCE_TYPE}" in
+      "namespace")
+        shift
+        NAME=$1
+        shift
+
+        read -r -p "To confirm deletion, type '${NAME}' and press <enter>: " CONFIRM_INPUT
+        if [ "${NAME}" == "${CONFIRM_INPUT}" ]; then
+          ${KUBECTL} "${COMMAND}" "${RESOURCE_TYPE}" "${NAME}" $@
+        else
+          echo "Cancel deletion."
+        fi
+        exit
+        ;;
+
+      *)
+        ${KUBECTL} "${COMMAND}" $@
+        exit
+        ;;
+    esac
+    ;;
+
   *)
     ${KUBECTL} $@
     exit
